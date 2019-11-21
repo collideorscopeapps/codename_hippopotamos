8e9aa3eb36a05e2d3128e3f94b82349314df7d21
MM <collideorscopeapps@gmail.com>
2019-11-21 13:41:17 +0100

    added quote activity (empty), intent da Main activity

--- app/src/main/java/it/collideorscopeapps/codename_hippopotamos/MainActivity.java
+++ app/src/main/java/it/collideorscopeapps/codename_hippopotamos/MainActivity.java
@@ -2,6 +2,7 @@ package it.collideorscopeapps.codename_hippopotamos;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +11,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.openQuoteActivity();
+    }
+
+    void openQuoteActivity() {
+
+        Intent intent = new Intent(MainActivity.this, QuoteActivity.class);
+
+        startActivity(intent);
     }
 }
