be72f5b6e921a0643768a460f733ec3660491238
MM <collideorscopeapps@gmail.com>
2019-11-21 11:51:00 +0100

    basic empty activity con android studio

---
+++ app/src/main/java/it/collideorscopeapps/codename_hippopotamos/MainActivity.java
@@ -0,0 +1,14 @@
+package it.collideorscopeapps.codename_hippopotamos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
