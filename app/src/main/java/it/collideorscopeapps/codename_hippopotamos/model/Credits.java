b8073e3f1988dad8b1393b1aa435a736532dd9e7
MM <collideorscopeapps@gmail.com>
2019-12-07 00:10:58 +0100

    Added credits table

---
+++ app/src/main/java/it/collideorscopeapps/codename_hippopotamos/model/Credits.java
@@ -0,0 +1,14 @@
+package it.collideorscopeapps.codename_hippopotamos.model;
+
+import java.util.ArrayList;
+
+public class Credits {
+
+    public ArrayList<String> credits;
+
+    public Credits() {
+
+        this.credits = new ArrayList<String>();
+    }
+
+}
