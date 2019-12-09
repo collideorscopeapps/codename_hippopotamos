5e40e00fae96030af086aa6b48193b18595bd8df
MM <collideorscopeapps@gmail.com>
2019-12-09 10:35:57 +0100

    added todo notes

--- app/src/main/java/it/collideorscopeapps/codename_hippopotamos/QuoteActivity.java
+++ app/src/main/java/it/collideorscopeapps/codename_hippopotamos/QuoteActivity.java
@@ -6,6 +6,10 @@ import android.os.Bundle;
 
 public class QuoteActivity extends AppCompatActivity {
 
+    // todo, at activity startup, get schermata with "audio test"
+    // iterate the quotes, get the audio files from assets folder
+    // play the ogg vorbis files
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
