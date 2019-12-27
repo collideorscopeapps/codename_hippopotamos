3ffd2b1ab3c09f3888fe01b6ef1027fd30a6133d
MM <collideorscopeapps@gmail.com>
2019-12-27 23:33:10 +0100

    removed unused import

--- app/src/main/java/it/collideorscopeapps/codename_hippopotamos/QuoteActivity.java
+++ app/src/main/java/it/collideorscopeapps/codename_hippopotamos/QuoteActivity.java
@@ -13,7 +13,6 @@ import android.widget.FrameLayout;
 import android.widget.TextView;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.TreeMap;
 
 import it.collideorscopeapps.codename_hippopotamos.database.AudioPlayerHelper;
