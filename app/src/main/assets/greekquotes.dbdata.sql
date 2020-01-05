f90d46a248485123c60f088f910974889d255799
MM <collideorscopeapps@gmail.com>
2020-01-05 10:14:27 +0100

    updated playlist names

--- app/src/main/assets/greekquotes.dbdata.sql
+++ app/src/main/assets/greekquotes.dbdata.sql
@@ -408,12 +408,12 @@ INSERT INTO "playlists" ("_id","description") VALUES (0,'Prepositions');
 INSERT INTO "playlists_schermate" ("playlist_id","schermata_id") VALUES (0,3);
 INSERT INTO "playlists_schermate" ("playlist_id","schermata_id") VALUES (0,59);
 
-INSERT INTO "playlists" ("_id","description","disabled") VALUES (1,'Parole, vocabolario, declinazioni',1);
+INSERT INTO "playlists" ("_id","description","disabled") VALUES (1,'Words, declensions',1);
 INSERT INTO "playlists_schermate" ("playlist_id","schermata_id") VALUES (1,1);
 INSERT INTO "playlists_schermate" ("playlist_id","schermata_id") VALUES (1,2);
 INSERT INTO "playlists_schermate" ("playlist_id","schermata_id") VALUES (1,4);
 
-INSERT INTO "playlists" ("_id","description","disabled") VALUES (2,'Frasi celebri',1);
+INSERT INTO "playlists" ("_id","description","disabled") VALUES (2,'Famous quotes',1);
 INSERT INTO "playlists_schermate" ("playlist_id","schermata_id") VALUES (2,3);
 
 INSERT INTO "playlists" ("_id","description") VALUES (3,'Reading list - word series for ear training');
