a0c57c1ff2dddfc910cd3644656ad33c29d00bcc
MM <collideorscopeapps@gmail.com>
2020-01-06 16:57:36 +0100

    more ui automation sample

--- app/src/androidTest/java/it/collideorscopeapps/codename_hippopotamos/UITests/HippoUIAutomatorTest.java
+++ app/src/androidTest/java/it/collideorscopeapps/codename_hippopotamos/UITests/HippoUIAutomatorTest.java
@@ -24,6 +24,9 @@ import androidx.test.runner.lifecycle.ActivityLifecycleMonitor;
 import androidx.test.runner.lifecycle.ActivityLifecycleMonitorRegistry;
 import androidx.test.uiautomator.UiDevice;
 import androidx.test.uiautomator.By;
+import androidx.test.uiautomator.UiObject;
+import androidx.test.uiautomator.UiObjectNotFoundException;
+import androidx.test.uiautomator.UiSelector;
 import androidx.test.uiautomator.Until;
 
 import static org.hamcrest.Matchers.equalTo;
@@ -91,5 +94,56 @@ public class HippoUIAutomatorTest {
         // Wait for the app to appear
         device.wait(Until.hasObject(By.pkg(BASIC_SAMPLE_PACKAGE).depth(0)),
                 LAUNCH_TIMEOUT);
+
+        //interact with app
+        UiObject cancelButton = device.findObject(new UiSelector()
+                .text("Cancel")
+                .className("android.widget.Button"));
+        UiObject okButton = device.findObject(new UiSelector()
+                .text("OK")
+                .className("android.widget.Button"));
+
+        // Simulate a user-click on the OK button, if found.
+        try {
+            if(okButton.exists() && okButton.isEnabled()) {
+                okButton.click();
+            }
+        } catch (UiObjectNotFoundException e) {
+
+        }
+
+        // As a best practice, when specifying a selector, you
+        // should use a Resource ID (if one is assigned to a UI
+        // element) instead of a text element or content-descriptor.
+        UiObject appItem = device.findObject(new UiSelector()
+                .className("android.widget.ListView")
+                .instance(0)
+                .childSelector(new UiSelector()
+                        .text("Apps")));
+
+        //appItem. swipeLeft();
     }
+
+    public void alternativeSetUp() {
+    //...
+
+        final String HIPPO_PACKAGE = "it.collideorscopeapps.codename_hippopotamos";
+        final int TIMEOUT = 500;
+
+        // Launch a simple calculator app
+        Context context = getInstrumentation().getContext();
+        Intent intent = context.getPackageManager()
+                .getLaunchIntentForPackage(HIPPO_PACKAGE);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
+
+        // Clear out any previous instances
+        context.startActivity(intent);
+        device.wait(Until.hasObject(By.pkg(HIPPO_PACKAGE).depth(0)), TIMEOUT);
+
+        //https://developer.android.com/training/testing/ui-testing/uiautomator-testing?authuser=1
+
+        //Testing fragments
+        //https://developer.android.com/training/basics/fragments/testing?authuser=1
+    }
+
 }
